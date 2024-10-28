50f3950d4737619b47dd4df1e4c5ad25bf82106e
wsdanshenmiao <wsdanshenmiao@outlook.com>
2024-10-28 18:07:39 +0800

    加入Shader后的渲染结果

--- DSMRenderer/Source File/TestShader.cpp
+++ DSMRenderer/Source File/TestShader.cpp
@@ -1,16 +1,24 @@
 #include "../Rendering/Tgaimage.h"
 #include "../Rendering/Rasterizer.h"
 #include "../Rendering/Shader/CommonShader.h"
+#include "../Math/Matrix/Matrix.h"
 
 using namespace DSM;
+using namespace DSM::Math;
 using namespace DSM::Rendering;
 
 void TestShader()
 {
 	std::shared_ptr renderTarget = std::make_shared<TGAImage>(512, 512, TGAImage::RGB);
-	std::shared_ptr model = std::make_shared<Model>("D:/Code/Computer Graphics/DSMRenderer/obj/Model/Elena.obj");
+	std::shared_ptr model = std::make_shared<Model>("D:/Code/Computer Graphics/DSMRenderer/obj/Model/dragon.obj");
 	std::unique_ptr shader = std::make_unique<CommonShader>();
 	shader->setDirectionLight({ { 0,0,1 } ,Color::white() });
+	Matrix4x4 world;
+	world << .1, 0, 0, 0,
+		0, .1, 0, 0,
+		0, 0, .1, 0,
+		0, 0, 0, 1;
+	shader->setWorldMatrix(world);
 	Rasterizer rasterizer(renderTarget, model, std::move(shader));
 	rasterizer.draw();
 	renderTarget->write_tga_file("../Output/output.tga");
